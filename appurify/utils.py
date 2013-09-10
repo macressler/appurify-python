f5159a580852c05cc08f302f4b91ae6cfefc95b7
Abhinav Singh <mailsforabhinav@gmail.com>
2013-09-10 22:24:41 +0530

    refactor utils get/post into AppurifyHttpClient, it now supports detection of api service down and try to live through downtime

--- appurify/utils.py
+++ appurify/utils.py
@@ -10,9 +10,10 @@
 """
 import os
 import sys
+import time
+import math
 import platform
 import requests
-import time
 import logging
 
 from . import constants
@@ -23,76 +24,168 @@ def log(msg, level=None): # pragma: no cover
     """simple logging facility"""
     logging.log(level if level else logging.WARNING, msg)
 
-def url(resource): # pragma: no cover
-    """(defaults: https://live.appurify.com:443/resource/)
+class AppurifyHttpClientError(Exception):
+    pass
 
-    Url can be overridden by specifying following environment variables
-    APPURIFY_API_PROTO (default: https)
-    APPURIFY_API_HOST (default: live.appurify.com)
-    APPURIFY_API_PORT (default: 443)
+class AppurifyHttpClient(object):
+    
+    def __init__(self, method, resource, payload=None, files=None, headers=None):
+        self.method_name = method
+        self.method = getattr(requests, self.method_name)
+        self.resource = resource
+        self.url = self.url(self.resource)
+        self.payload = payload
+        self.files = files
+        
+        if headers:
+            assert type(headers) == dict
+        else:
+            headers = dict()
+        self.headers = headers
+        self.headers['User-Agent'] = self.user_agent()
+        
+        self.retry_count = 0
+    
+    @staticmethod
+    def url(resource): # pragma: no cover
+        """(defaults: https://live.appurify.com:443/resource/)
+    
+        Url can be overridden by specifying following environment variables
+        APPURIFY_API_PROTO (default: https)
+        APPURIFY_API_HOST (default: live.appurify.com)
+        APPURIFY_API_PORT (default: 443)
+    
+        Clients and Customers MUST not override this unless instructed by Appurify devs
+        """
+        return '/'.join(['%s://%s:%s/resource' % (
+            os.environ.get('APPURIFY_API_PROTO', constants.API_PROTO),
+            os.environ.get('APPURIFY_API_HOST', constants.API_HOST),
+            os.environ.get('APPURIFY_API_PORT', str(constants.API_PORT))
+        ), resource]) + '/'
+    
+    @staticmethod
+    def user_agent(): # pragma: no cover
+        """returns string representation of user-agent"""
+        implementation = platform.python_implementation()
+    
+        if implementation == 'CPython':
+            version = platform.python_version()
+        elif implementation == 'PyPy':
+            version = '%s.%s.%s' % (sys.pypy_version_info.major, sys.pypy_version_info.minor, sys.pypy_version_info.micro)
+        elif implementation == 'Jython':
+            version = platform.python_version()
+        elif implementation == 'IronPython':
+            version = platform.python_version()
+        else:
+            version = 'Unknown'
+    
+        try:
+            system = platform.system()
+            release = platform.release()
+        except IOError:
+            system = 'Unknown'
+            release = 'Unknown'
+    
+        return " ".join([
+            'appurify-client/%s' % constants.__version__,
+            'python-requests/%s' % requests.__version__,
+            '%s/%s' % (implementation, version),
+            '%s/%s' % (system, release)
+        ])
+    
+    @property
+    def retry_on_failure(self):
+        return int(os.environ.get('APPURIFY_API_RETRY_ON_FAILURE', constants.API_RETRY_ON_FAILURE))
+    
+    @property
+    def max_retry(self):
+        return int(os.environ.get('APPURIFY_API_MAX_RETRY', constants.API_MAX_RETRY))
+    
+    @property
+    def retry_delay(self):
+        return int(os.environ.get('APPURIFY_API_RETRY_DELAY', constants.API_RETRY_DELAY))
+    
+    @staticmethod
+    def api_status():
+        """returns api service status from aws status page."""
+        return constants.API_STATUS_DOWN
+    
+    @staticmethod
+    def wait_for_api_service():
+        """waits endlessly until api service is up.
+        
+        .. todo::
+        
+            - support timeouts
+        """
+        n = 1
+        while AppurifyHttpClient.api_status() == constants.API_STATUS_DOWN:
+            # restrict max sleep to 2^6 ~ 1 min
+            delay = int(math.pow(2, n % 7))
+            log('Service is down, will retry in %s seconds...' % delay)
+            time.sleep(delay)
+            n += 1
+        log('API service is back up, resuming...')
+        return True
+    
+    def is_api_response(self, response):
+        """detects if response came from api backend or from higher up in the stack."""
+        return 'x-api-server-hostname' in response.headers
+    
+    def kwargs(self):
+        """returns kwargs for configured requests method."""
+        kwargs = dict()
+        
+        key = 'params' if self.method_name == 'get' else 'data'
+        kwargs[key] = self.payload
+        
+        kwargs['headers'] = self.headers
+        kwargs['verify'] = False
+        
+        if self.method == 'post':
+            kwargs['files'] = self.files
+        
+        return kwargs
+    
+    def start(self):
+        self.retry_count += 1
+        log("HTTP %s %s" % (self.method_name.upper(), self.url))
+        
+        try:
+            response = self.method(self.url, **self.kwargs())
+            if self.is_api_response(response):
+                # received response from api backend
+                if response.status_code == 200:
+                    return response
+            else:
+                # received response from lb
+                log('Received unexpected response from API, waiting for service to resume...')
+                self.wait_for_api_service()
+        except requests.exceptions.ConnectionError as e:
+            # either no internet connectivity / dns failures
+            # or lb is not responding/down
+            response = None
+            log('Connection to API server failed, waiting for service to resume...')
+            self.wait_for_api_service()
+        
+        if self.retry_on_failure and self.retry_count <= self.max_retry:
+            time.sleep(int(math.pow(2, self.retry_count)))
+            return self.start()
+        
+        if response:
+            raise AppurifyHttpClientError('API failure with response %s, code %s' % (response.text, response.status_code))
+        else:
+            raise AppurifyHttpClientError('API failure with reason %s' % str(e))
 
-    Clients and Customers MUST not override this unless instructed by Appurify devs
-    """
-    return '/'.join(['%s://%s:%s/resource' % (
-        os.environ.get('APPURIFY_API_PROTO', constants.API_PROTO),
-        os.environ.get('APPURIFY_API_HOST', constants.API_HOST),
-        os.environ.get('APPURIFY_API_PORT', str(constants.API_PORT))
-    ), resource]) + '/'
-
-def user_agent(): # pragma: no cover
-    """returns string representation of user-agent"""
-    implementation = platform.python_implementation()
-
-    if implementation == 'CPython':
-        version = platform.python_version()
-    elif implementation == 'PyPy':
-        version = '%s.%s.%s' % (sys.pypy_version_info.major, sys.pypy_version_info.minor, sys.pypy_version_info.micro)
-    elif implementation == 'Jython':
-        version = platform.python_version()
-    elif implementation == 'IronPython':
-        version = platform.python_version()
-    else:
-        version = 'Unknown'
-
-    try:
-        system = platform.system()
-        release = platform.release()
-    except IOError:
-        system = 'Unknown'
-        release = 'Unknown'
-
-    return " ".join([
-        'appurify-client/%s' % constants.__version__,
-        'python-requests/%s' % requests.__version__,
-        '%s/%s' % (implementation, version),
-        '%s/%s' % (system, release)
-    ])
-
-def get(resource, params, retry_count=0, retry=True): # pragma: no cover
+def get(resource, params): # pragma: no cover
     """make a HTTP GET request on API endpoint"""
-    log("HTTP GET %s" % url(resource))
-    response = requests.get(url(resource), params=params, verify=False, headers={'User-Agent': user_agent()})
-    if retry and response.status_code != 200 and \
-        int(os.environ.get('APPURIFY_API_RETRY_ON_FAILURE', constants.API_RETRY_ON_FAILURE)) == 1 and \
-        retry_count < int(os.environ.get('APPURIFY_API_MAX_RETRY', constants.API_MAX_RETRY)):
-        time.sleep(int(os.environ.get('APPURIFY_API_RETRY_DELAY', constants.API_RETRY_DELAY)))
-        retry_count += 1
-        return get(resource, params, retry_count, retry)
-    else:
-        return response
+    client = AppurifyHttpClient('get', resource, params)
+    return client.start()
 
-def post(resource, data, files=None, retry_count=0, retry=True): # pragma: no cover
+def post(resource, data, files=None): # pragma: no cover
     """make a HTTP POST request on API endpoint"""
-    log("HTTP POST %s" % url(resource))
-    response = requests.post(url(resource), data=data, files=files, verify=False, headers={'User-Agent': user_agent()})
-    if retry and response.status_code != 200 and \
-        int(os.environ.get('APPURIFY_API_RETRY_ON_FAILURE', constants.API_RETRY_ON_FAILURE)) == 1 and \
-        retry_count < int(os.environ.get('APPURIFY_API_MAX_RETRY', constants.API_MAX_RETRY)):
-        time.sleep(int(os.environ.get('APPURIFY_API_RETRY_DELAY', constants.API_RETRY_DELAY)))
-        retry_count += 1
-        return post(resource, data, files, retry_count, retry)
-    else:
-        return response
+    client = AppurifyHttpClient('post', resource, data, files=files)
+    return client.start()
 
 def wget(url, path): # pragma: no cover
     """Download a file to specified path"""
